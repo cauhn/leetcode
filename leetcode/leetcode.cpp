6e65df1cbc29079dee7e54fb1e83ebc1ba1359c8
cauhn <cauhn@qq.com>
2013-08-15 22:33:43 +0800

    find substring

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -5,7 +5,9 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <math.h>
 #include <string>
+#include <set>
 
 using namespace std;
 
@@ -18,6 +20,267 @@ struct ListNode {
 
 class Solution {
 public:
+	//查找级联容器中的字符串在S中的位置
+	vector<int> findSubstring(string S, vector<string> &L) {
+     // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+		vector<int>	res;
+		if(L.empty())	return res;
+		size_t sz = L.begin()->size();
+		multiset<string>	strset(L.begin(),L.end()),tmp(strset);
+		int sumsz = sz*L.size();
+
+
+	for(int i=0; i<sz; i++)
+		{
+			int ind = i;
+			vector<string>		subvec;
+			multiset<string>	tmpset(strset);
+			//对字符串分串
+			while(ind<S.size())
+			{
+				string str = S.substr(ind,sz);
+				subvec.push_back(str);
+				ind = ind+sz;
+			}
+
+			int 
+			while(!tmpset.empty())
+			{
+				
+			
+			}
+		
+		}
+		return res;
+
+		//for(int i=0; i<S.size();i++ )
+		//{
+		//	tmp = strset;
+		//	if(i+sumsz>S.size())
+		//		break;
+		//	int ind =i;
+		//	while(!tmp.empty())
+		//	{
+		//		string sub = S.substr(ind,sz);
+		//		multiset<string>::iterator iter = tmp.find(sub);
+		//		if(iter==tmp.end())
+		//			break;
+		//		else
+		//		{
+		//			ind = ind+sz;
+		//			tmp.erase(iter);
+		//		}
+		//	}
+		//	//判断是否全部找到
+		//	if(tmp.empty())
+		//	{
+		//		int index = ind-sumsz;
+		//		res.push_back(index);
+		//	}
+		//}
+		//return res;
+		
+    }
+	//除法 
+	int divide(int dividend, int divisor) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        bool flag = true;
+		if(divisor==1 )	return dividend;
+		if(divisor==0)	return 0;
+		if((divisor>0 && dividend<0 ) || (divisor<0 && dividend>0))
+			flag = false;
+
+		long long u_divisor, u_dividend, big ,small, result, sum=0;
+		
+		u_divisor = abs((long double)divisor);
+		u_dividend = abs((long double)dividend);
+
+
+		while(u_dividend>u_divisor)
+		{
+			big = u_dividend;
+			small = u_divisor;
+			result = 1;
+
+			while(small<big)
+			{
+				small = small<<1;
+				result = result<<1;
+			}
+			if(small > big)
+			{
+				small = small>>1;
+				result = result >>1;
+			}
+			u_dividend = u_dividend - small;
+			sum = sum+result;
+		}
+		
+		if(u_dividend == u_divisor)
+			sum = sum+1;
+		
+
+		if(!flag)	return -sum;
+		else return sum;
+
+    }
+	//KMP算法
+	char *strStr(char *haystack, char *needle) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
+        int m = strlen(needle);
+    	int* move = new int[m];
+		compute_prefixfun(needle,move);
+		int q=-1;
+		int n = strlen(haystack);
+		if(m==0)    return haystack;
+        
+        for(int i=0; i<n;i++)
+		{
+			while(q>-1 && haystack[i]!=needle[q+1])
+				q = move[q];
+			if(haystack[i]==needle[q+1])
+				q = q+1;
+			if(q==m-1)
+				return haystack+i-m+1;
+		}
+		return NULL;
+    }
+	
+	void compute_prefixfun(char* needle, int* move)
+	{
+		int m = strlen(needle);
+		move[0]= -1;
+		int k=-1;
+		for(int i=1; i<m-1; i++)
+		{
+			while(k>-1 && needle[k+1] !=needle[i])
+				k = move[k];
+			if(needle[k+1]==needle[i])
+				k++;
+			move[i] = k;
+		}
+	}
+
+
+	//删除数组中与elem值相同的值，同时返回最后长度
+	int removeElement(int A[], int n, int elem) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int ind,cnt;
+		for(ind=0,cnt=0; cnt!=n; cnt++)
+		{
+			if(A[cnt]==elem)
+			{
+				continue;
+			}
+			else
+			{
+				A[ind++] = A[cnt];
+			}
+		}
+
+		return ind;
+    }
+
+	//原地删除重复数据  数组已排序
+    int removeDuplicates(int A[], int n) {
+    // Start typing your C/C++ solution below
+    // DO NOT write int main() function
+		int ind =1;
+		int ptr = 1;
+		if(n<2)	return n;
+		for(; ptr!=n; ptr++)
+		{
+			if(A[ptr]==A[ptr-1])
+			{
+				continue;
+			}
+			else
+			{
+				A[ind] = A[ptr];
+				ind++;
+			}
+		}
+		return ind;
+	}
+	//两两交换指针，不能改变数据
+    ListNode *swapPairs(ListNode *head) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        ListNode* first = new ListNode(0);
+		first->next = head;
+		
+		ListNode *pre, *cur, *nex;
+		pre = first;
+		cur = head;
+		while(cur && cur->next)
+		{
+			nex = cur->next;
+
+			pre->next = nex;
+			cur->next = nex->next;
+			nex->next = cur;
+			
+			pre = cur;
+			cur = cur->next;
+		}
+		return first->next;
+
+    }
+	
+	//每K个节点反转，最后不足K节点的不做反转
+	ListNode *reverseKGroup(ListNode *head, int k) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        ListNode* first = new ListNode(0);
+		first->next = head;
+
+		ListNode* pre = first;
+		ListNode *p, *q,*r,*tail;
+		while(pre->next)
+		{
+			tail = pre->next;
+			p = pre->next;
+			q = p->next;
+			int cnt ;
+			for(cnt=1; cnt<k && q ;cnt++)
+			{
+				r = q->next;
+				q->next = p;
+				p = q;
+				q = r;
+			}
+			//前面N组
+			if(cnt==k)
+			{
+				pre->next = p;
+				pre = tail;
+				pre->next = q;
+			}
+			//最后一组
+			else
+			{
+				while(cnt!=1)
+				{
+					r = p->next;
+					p->next = q;
+					q = p;
+					p = r;
+					cnt--;
+				}
+				break;
+			}
+		}
+		return first->next;
+    }
+	
+
+
+
     ListNode *mergeKLists(vector<ListNode *> &lists) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
@@ -99,6 +362,19 @@ public:
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+	Solution s;
+	int i = (int)2147483648*-1;
+	int c;
+	if(i>=0L)
+	{
+		printf("hello\n");
+	}
+	else
+		 c = -i;
+	int d = i>=0L ? i : -i;
+	//int d = abs(i);
+	s.divide(i,2);
 	return 0;
 }
 
